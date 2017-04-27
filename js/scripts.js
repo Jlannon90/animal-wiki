be7bec865e7bc1f238baeb42cae5c46742454b1c
James Lannon and Kristen McKee <james@>
2017-04-27 09:51:09 -0700

    add branching in javascript and jQuery functions, add font and styling to css.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
-var animal = $("input:radio[name=animal]:checked").val();
+$(function(){
+  $(".radio").click(function(event){
+    var animal = $("input:radio[name=animal]:checked").val();
+  // $(this).children().toggleClass(".animals");
+
+  if (animal === "baby_elephant") {
+    $(".baby-elephant").show();
+    $(".red-panda").hide();
+    $(".munchkin-cat").hide();
+  } else if (animal === "red_panda") {
+    $(".red-panda").show();
+    $(".baby-elephant").hide();
+    $(".munchkin-cat").hide();
+  } else {
+    $(".munchkin-cat").show();
+    $(".baby-elephant").hide();
+    $(".red-panda").hide();
+  }
+
+
+
+
+  event.preventDefault();
+  });
+});
